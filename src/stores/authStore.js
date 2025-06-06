532a7437d567dd189a3a362c0ab1d615ffdd61f4
armmani <arm.maniii@gmail.com>
2025-06-06 20:09:07 +0700

    จบauthStore

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -2,10 +2,18 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import authApi from "../api/authApi";
 
-const useAuthStore = create(persist( (set) = ({
-  user: null,
-  accessToken: "",
-  actionLogin: async (input) => {
-    await authApi.login(input)
-  }
-}) ))
+const useAuthStore = create(
+  persist((set) = ({
+    userId: null,
+    accessToken: "",
+    actionLogin: async (input) => {
+      const res = await authApi.login(input);
+      console.log("res login", res.data)
+      set({ userId: res.data.userId, accessToken: res.data.accessToken })
+    },
+  }), {
+    name: "user-store",
+  })
+);
+
+export default useAuthStore
