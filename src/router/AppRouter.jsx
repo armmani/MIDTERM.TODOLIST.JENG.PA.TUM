1dfe01ff659f70a66744303ea900d69b0655c2b0
armmani <arm.maniii@gmail.com>
2025-06-06 17:37:37 +0700

    สร้าง Router ใน AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import TodoListPage from '../pages/TodoListPage';
+import MainLayout from '../layout/MainLayout';
+import LoginPage from '../pages/LoginPage';
 
 function AppRouter() {
   return (
-    <div>AppRouter</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<LoginPage />}/>
+          <Route path="todo" element={<TodoListPage />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default AppRouter
